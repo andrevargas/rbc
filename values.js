2e93c71caebfd806d4fadfb8893cd51d3f5be0ea
André Vargas <andrelv510@gmail.com>
2017-05-01 23:20:24 -0300

    :zzz:

---
+++ values.js
@@ -0,0 +1,42 @@
+const SO_VALUES = {
+    'Android': 0,
+    'iOS': 0,
+    'Windows Phone': 0
+};
+
+const SCREEN_SIZE_VALUES = {
+    '5.0': 0,
+    '5.5': 0,
+    '5.7': 0,
+    '5.8': 0,
+    '5.15': 0
+};
+
+const CORES_VALUES = {
+    '2': 0,
+    '4': 0,
+    '8': 0
+};
+
+const RAM_VALUES = {
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '6': 0,
+    '8': 0
+};
+
+const STORAGE_VALUES = {
+    '16': 0,
+    '32': 0,
+    '64': 0,
+    '128': 0,
+    '256': 0
+};
+
+const PRICE_RANGE_VALUES = {
+    '0-999': 0,
+    '1000-1999': 0,
+    '2000-2999': 0,
+    '3000': 0 
+};
